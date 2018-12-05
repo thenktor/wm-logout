d9718e0a94634ace44c158b9ac328ba3e66f1d3c
Thorsten <thenktor@users.noreply.github.com>
2018-12-05 17:11:04 +0100

    Added PREFIX variable

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-install -d -m 755 $DESTDIR/usr/bin
-install -m755 wm-logout $DESTDIR/usr/bin/
+PREFIX="/usr/local"
+
+install -d -m 755 $DESTDIR/$PREFIX/bin
+install -m755 wm-logout $DESTDIR/$PREFIX/bin/
